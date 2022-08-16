d586b630f73f66acb01c11b5fa3a7925983c00b6
jawoznia <jnwznk@gmail.com>
2022-08-16 13:35:41 +0200

    Query and Execute tested.

--- contracts/cw1/src/lib.rs
+++ contracts/cw1/src/lib.rs
@@ -1,32 +1,50 @@
 #![allow(dead_code)]
-use cosmwasm_std::{CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdError};
+use cosmwasm_std::{Deps, DepsMut, Env, MessageInfo, Response, StdError};
 
 use sylvia_derive::interface;
 
 #[interface(module=msg)]
-pub trait Cw1<Msg>
-where
-    Msg: std::fmt::Debug + PartialEq + Clone + schemars::JsonSchema,
-{
+pub trait Cw1 {
     type Error: From<StdError>;
 
     #[msg(exec)]
-    fn execute(
+    fn add_member(
         &self,
         ctx: (DepsMut, Env, MessageInfo),
-        msgs: Vec<CosmosMsg<Msg>>,
+        member: String,
     ) -> Result<Response, Self::Error>;
 
     #[msg(query)]
-    fn query(&self, ctx: (Deps, Env), addr: String) -> Result<Response, Self::Error>;
+    fn find_member(&self, ctx: (Deps, Env), member: String) -> Result<Response, Self::Error>;
 }
 
 #[cfg(test)]
 mod tests {
+    use cosmwasm_std::{from_binary, to_binary};
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
+    fn execute() {
+        let original_msg = msg::ExecMsg::AddMember {
+            member: "member_name".to_owned(),
+        };
+
+        let serialized_msg = to_binary(&original_msg).unwrap();
+        let serialized_msg: msg::ExecMsg = from_binary(&serialized_msg).unwrap();
+
+        assert_eq!(serialized_msg, original_msg);
+    }
+
+    #[test]
+    fn query() {
+        let original_msg = msg::QueryMsg::FindMember {
+            member: "member_name".to_owned(),
+        };
+
+        let serialized_msg = to_binary(&original_msg).unwrap();
+        let serialized_msg: msg::QueryMsg = from_binary(&serialized_msg).unwrap();
+
+        assert_eq!(serialized_msg, original_msg);
     }
 }
