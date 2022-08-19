f369cd6ada655a3733c34d498e8dfc9db403e8e8
jawoznia <jnwznk@gmail.com>
2022-08-19 17:55:32 +0200

    anyhow and cw-multi-test moved to dev-dependencies.

--- contracts/cw1-whitelist/src/multitest.rs
+++ contracts/cw1-whitelist/src/multitest.rs
@@ -1,59 +1,53 @@
-use anyhow::{bail, Result as AnyResult};
-use cosmwasm_std::{Binary, DepsMut, Empty, Env, MessageInfo, Reply, Response};
-use cw_multi_test::Contract;
-
-use crate::contract::Cw1WhitelistContract;
-
-impl Contract<Empty> for Cw1WhitelistContract {
-    fn instantiate(
-        &self,
-        deps: DepsMut,
-        env: Env,
-        info: MessageInfo,
-        msg: Vec<u8>,
-    ) -> AnyResult<Response<Empty>> {
-        self.entry_instantiate(deps, env, info, &msg)
-            .map_err(Into::into)
-    }
-
-    fn execute(
-        &self,
-        deps: DepsMut,
-        env: Env,
-        info: MessageInfo,
-        msg: Vec<u8>,
-    ) -> AnyResult<Response<Empty>> {
-        self.entry_execute(deps, env, info, &msg)
-            .map_err(Into::into)
-    }
-
-    fn query(&self, deps: cosmwasm_std::Deps, env: Env, msg: Vec<u8>) -> AnyResult<Binary> {
-        self.entry_query(deps, env, &msg).map_err(Into::into)
-    }
-
-    fn sudo(&self, _deps: DepsMut, _env: Env, _msg: Vec<u8>) -> AnyResult<Response<Empty>> {
-        bail!("sudo not implemented for contract")
-    }
-
-    fn reply(&self, _deps: DepsMut, _env: Env, _msg: Reply) -> AnyResult<Response<Empty>> {
-        bail!("reply not implemented for contract")
-    }
-
-    fn migrate(&self, _deps: DepsMut, _env: Env, _msg: Vec<u8>) -> AnyResult<Response<Empty>> {
-        bail!("migrate not implemented for contract")
-    }
-}
-
 #[cfg(test)]
 mod test {
-    use cosmwasm_std::Addr;
-    use cw_multi_test::{AppBuilder, Executor};
-
     use crate::contract::{contract::InstantiateMsg, Cw1WhitelistContract};
+    use anyhow::{bail, Result as AnyResult};
+    use cosmwasm_std::{Addr, Binary, DepsMut, Empty, Env, MessageInfo, Reply, Response};
     use cw1::{
         msg::{ExecMsg, QueryMsg},
         FindMemberResponse,
     };
+    use cw_multi_test::{AppBuilder, Contract, Executor};
+
+    impl Contract<Empty> for Cw1WhitelistContract {
+        fn instantiate(
+            &self,
+            deps: DepsMut,
+            env: Env,
+            info: MessageInfo,
+            msg: Vec<u8>,
+        ) -> AnyResult<Response<Empty>> {
+            self.entry_instantiate(deps, env, info, &msg)
+                .map_err(Into::into)
+        }
+
+        fn execute(
+            &self,
+            deps: DepsMut,
+            env: Env,
+            info: MessageInfo,
+            msg: Vec<u8>,
+        ) -> AnyResult<Response<Empty>> {
+            self.entry_execute(deps, env, info, &msg)
+                .map_err(Into::into)
+        }
+
+        fn query(&self, deps: cosmwasm_std::Deps, env: Env, msg: Vec<u8>) -> AnyResult<Binary> {
+            self.entry_query(deps, env, &msg).map_err(Into::into)
+        }
+
+        fn sudo(&self, _deps: DepsMut, _env: Env, _msg: Vec<u8>) -> AnyResult<Response<Empty>> {
+            bail!("sudo not implemented for contract")
+        }
+
+        fn reply(&self, _deps: DepsMut, _env: Env, _msg: Reply) -> AnyResult<Response<Empty>> {
+            bail!("reply not implemented for contract")
+        }
+
+        fn migrate(&self, _deps: DepsMut, _env: Env, _msg: Vec<u8>) -> AnyResult<Response<Empty>> {
+            bail!("migrate not implemented for contract")
+        }
+    }
 
     #[test]
     fn entry_points() {
