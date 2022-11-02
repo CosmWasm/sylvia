1f0cbc14650751188ba151633caaa3a9252bbcdb
jawoznia <jnwznk@gmail.com>
2022-11-02 12:45:42 +0100

    cw20-allowances proxy

--- contracts/cw20-base/src/multitest/proxy.rs
+++ contracts/cw20-base/src/multitest/proxy.rs
@@ -1,5 +1,8 @@
 use cosmwasm_std::{Addr, BalanceResponse, Binary, Coin, CosmosMsg, Response, StdResult, Uint128};
-use cw20::TokenInfoResponse;
+use cw20::{
+    AllAllowancesResponse, AllSpenderAllowancesResponse, AllowanceResponse, TokenInfoResponse,
+};
+use cw20_allowances::Cw20Allowances;
 use cw_multi_test::{App, Executor};
 use cw_utils::Expiration;
 
@@ -41,6 +44,7 @@ impl Cw1SubkeysProxy {
         &self.0
     }
 
+    // cw20-base
     #[track_caller]
     pub fn transfer(
         &self,
@@ -93,6 +97,147 @@ impl Cw1SubkeysProxy {
 
     pub fn token_info(&self, app: &App) -> StdResult<TokenInfoResponse> {
         let msg = QueryMsg::TokenInfo {};
+
+        app.wrap().query_wasm_smart(self.0.clone(), &msg)
+    }
+
+    // cw20-allowances
+    #[track_caller]
+    pub fn increase_allowance(
+        &self,
+        app: &mut App,
+        sender: &Addr,
+        spender: String,
+        amount: Uint128,
+        expires: Option<Expiration>,
+    ) -> Result<(), ContractError> {
+        let msg = cw20_allowances::ExecMsg::IncreaseAllowance {
+            spender,
+            amount,
+            expires,
+        };
+
+        app.execute_contract(sender.clone(), self.0.clone(), &msg, &[])
+            .map_err(|err| err.downcast().unwrap())
+            .map(|_| ())
+    }
+
+    #[track_caller]
+    pub fn decrease_allowance(
+        &self,
+        app: &mut App,
+        sender: &Addr,
+        spender: String,
+        amount: Uint128,
+        expires: Option<Expiration>,
+    ) -> Result<(), ContractError> {
+        let msg = cw20_allowances::ExecMsg::DecreaseAllowance {
+            spender,
+            amount,
+            expires,
+        };
+
+        app.execute_contract(sender.clone(), self.0.clone(), &msg, &[])
+            .map_err(|err| err.downcast().unwrap())
+            .map(|_| ())
+    }
+
+    #[track_caller]
+    pub fn transfer_from(
+        &self,
+        app: &mut App,
+        sender: &Addr,
+        owner: String,
+        recipient: String,
+        amount: Uint128,
+    ) -> Result<(), ContractError> {
+        let msg = cw20_allowances::ExecMsg::TransferFrom {
+            owner,
+            recipient,
+            amount,
+        };
+
+        app.execute_contract(sender.clone(), self.0.clone(), &msg, &[])
+            .map_err(|err| err.downcast().unwrap())
+            .map(|_| ())
+    }
+
+    #[track_caller]
+    pub fn send_from(
+        &self,
+        app: &mut App,
+        sender: &Addr,
+        owner: String,
+        contract: String,
+        amount: Uint128,
+        msg: Binary,
+    ) -> Result<(), ContractError> {
+        let msg = cw20_allowances::ExecMsg::SendFrom {
+            owner,
+            contract,
+            amount,
+            msg,
+        };
+
+        app.execute_contract(sender.clone(), self.0.clone(), &msg, &[])
+            .map_err(|err| err.downcast().unwrap())
+            .map(|_| ())
+    }
+
+    #[track_caller]
+    pub fn burn_from(
+        &self,
+        app: &mut App,
+        sender: &Addr,
+        owner: String,
+        amount: Uint128,
+    ) -> Result<(), ContractError> {
+        let msg = cw20_allowances::ExecMsg::BurnFrom { owner, amount };
+
+        app.execute_contract(sender.clone(), self.0.clone(), &msg, &[])
+            .map_err(|err| err.downcast().unwrap())
+            .map(|_| ())
+    }
+
+    pub fn allowance<'a>(
+        &self,
+        app: &App,
+        owner: String,
+        spender: String,
+    ) -> StdResult<AllowanceResponse> {
+        let msg = cw20_allowances::Cw20AllowancesQueryMsg::Allowance { owner, spender };
+
+        app.wrap().query_wasm_smart(self.0.clone(), &msg)
+    }
+
+    pub fn all_allowances<'a>(
+        &self,
+        app: &App,
+        owner: String,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    ) -> StdResult<AllAllowancesResponse> {
+        let msg = cw20_allowances::Cw20AllowancesQueryMsg::AllAllowances {
+            owner,
+            start_after,
+            limit,
+        };
+
+        app.wrap().query_wasm_smart(self.0.clone(), &msg)
+    }
+
+    pub fn all_spender_allowances<'a>(
+        &self,
+        app: &App,
+        spender: String,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    ) -> StdResult<AllSpenderAllowancesResponse> {
+        let msg = cw20_allowances::Cw20AllowancesQueryMsg::AllSpenderAllowances {
+            spender,
+            start_after,
+            limit,
+        };
 
         app.wrap().query_wasm_smart(self.0.clone(), &msg)
     }
