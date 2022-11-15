922c087a04bb9b9ecb4a4a6183d71a6dc63406eb
jawoznia <jnwznk@gmail.com>
2022-11-15 16:48:37 +0100

    Schema generation available on cw20-base

---
+++ contracts/cw20-base/src/bin/schema.rs
@@ -0,0 +1,12 @@
+use cosmwasm_schema::write_api;
+
+use cw20_base::contract::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+
+#[cfg(not(tarpaulin_include))]
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ContractExecMsg,
+        query: ContractQueryMsg,
+    }
+}
