c0c626281718beaa873898f43298ded9c5c49a21
Jan Woźniak <jnwznk@gmail.com>
2023-04-05 14:26:46 +0200

    Switch library feature to no_entry_points

--- sylvia-derive/src/message.rs
+++ sylvia-derive/src/message.rs
@@ -860,7 +860,7 @@ impl<'a> EntryPoints<'a> {
     }
 
     pub fn emit(&self) -> TokenStream {
-        if cfg!(feature = "library") {
+        if cfg!(feature = "no_entry_points") {
             return quote! {};
         }
         let Self { name, error } = self;
