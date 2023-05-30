cc397dd810156e05d9083f62c6f496daae4c0815
Jan Woźniak <jnwznk@gmail.com>
2023-05-30 10:22:52 +0200

    Error handling in IntoResponse

--- sylvia/src/into_response.rs
+++ sylvia/src/into_response.rs
@@ -1,50 +1,62 @@
-use cosmwasm_std::{CosmosMsg, Empty, Response, SubMsg};
+use cosmwasm_std::{CosmosMsg, Empty, Response, StdError, StdResult, SubMsg};
 
 /// Trait converting `SubMsg` to one carrying another chain-custom message
 pub trait IntoMsg<C> {
-    fn into_msg(self) -> SubMsg<C>;
+    fn into_msg(self) -> StdResult<SubMsg<C>>;
 }
 
 /// `SubMsg<Empty>` can be made into any `SubMsg<C>`
 impl<C> IntoMsg<C> for SubMsg<Empty> {
-    fn into_msg(self) -> SubMsg<C> {
-        SubMsg {
-            msg: match self.msg {
-                CosmosMsg::Wasm(wasm) => CosmosMsg::Wasm(wasm),
-                CosmosMsg::Bank(bank) => CosmosMsg::Bank(bank),
-                CosmosMsg::Staking(staking) => CosmosMsg::Staking(staking),
-                CosmosMsg::Distribution(distribution) => CosmosMsg::Distribution(distribution),
-                CosmosMsg::Custom(_) => unreachable!(),
-                CosmosMsg::Ibc(ibc) => CosmosMsg::Ibc(ibc),
-                CosmosMsg::Stargate { type_url, value } => CosmosMsg::Stargate { type_url, value },
-                _ => panic!("unknown message variant {:?}", self),
-            },
+    fn into_msg(self) -> StdResult<SubMsg<C>> {
+        let msg = match self.msg {
+            CosmosMsg::Wasm(wasm) => CosmosMsg::Wasm(wasm),
+            CosmosMsg::Bank(bank) => CosmosMsg::Bank(bank),
+            CosmosMsg::Staking(staking) => CosmosMsg::Staking(staking),
+            CosmosMsg::Distribution(distribution) => CosmosMsg::Distribution(distribution),
+            CosmosMsg::Custom(_) => Err(StdError::generic_err(
+                "Custom Empty message should not be sent",
+            ))?,
+            CosmosMsg::Ibc(ibc) => CosmosMsg::Ibc(ibc),
+            CosmosMsg::Stargate { type_url, value } => CosmosMsg::Stargate { type_url, value },
+            _ => Err(StdError::generic_err(format!(
+                "Unknown message variant: {:?}",
+                self
+            )))?,
+        };
+
+        Ok(SubMsg {
+            msg,
             id: self.id,
             gas_limit: self.gas_limit,
             reply_on: self.reply_on,
-        }
+        })
     }
 }
 
 pub trait IntoResponse<T> {
-    fn into_response(self) -> Response<T>;
+    fn into_response(self) -> StdResult<Response<T>>;
 }
 
 impl<T> IntoResponse<T> for Response<Empty> {
-    fn into_response(self) -> Response<T> {
+    fn into_response(self) -> StdResult<Response<T>> {
+        let messages = self
+            .messages
+            .into_iter()
+            .map(|msg| msg.into_msg())
+            .collect::<StdResult<Vec<_>>>()?;
         let mut resp = Response::new()
-            .add_submessages(self.messages.into_iter().map(IntoMsg::into_msg))
+            .add_submessages(messages.into_iter())
             .add_events(self.events)
             .add_attributes(self.attributes);
         resp.data = self.data;
 
-        resp
+        Ok(resp)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::{CustomMsg, Empty, Response};
+    use cosmwasm_std::{CosmosMsg, CustomMsg, Empty, Response, StdError};
     use schemars::JsonSchema;
     use serde::{Deserialize, Serialize};
 
@@ -59,6 +71,18 @@ mod tests {
     fn into_custom() {
         let resp = Response::<Empty>::default();
 
-        let _: Response<MyMsg> = resp.into_response();
+        let _: Response<MyMsg> = resp.into_response().unwrap();
+    }
+
+    #[test]
+    fn empty_custom_msg() {
+        let mut resp = Response::<Empty>::default();
+        resp = resp.add_message(CosmosMsg::Custom(Empty {}));
+
+        let err = IntoResponse::<MyMsg>::into_response(resp).unwrap_err();
+        assert_eq!(
+            err,
+            StdError::generic_err("Custom Empty message should not be sent")
+        );
     }
 }
