629f3a2fe1bcde3d40c01b7809ac70deb54c6bc2
Jan Woźniak <jnwznk@gmail.com>
2023-09-27 17:45:12 +0200

    feat: Support generic interface implemented on contract

--- sylvia-derive/src/check_generics.rs
+++ sylvia-derive/src/check_generics.rs
@@ -1,26 +1,57 @@
 use syn::visit::Visit;
-use syn::GenericParam;
+use syn::{parse_quote, GenericArgument, GenericParam, Type};
 
-#[derive(Debug)]
-pub struct CheckGenerics<'g> {
-    generics: &'g [&'g GenericParam],
-    used: Vec<&'g GenericParam>,
+pub trait GetPath {
+    fn get_path(&self) -> Option<syn::Path>;
 }
 
-impl<'g> CheckGenerics<'g> {
-    pub fn new(generics: &'g [&'g GenericParam]) -> Self {
+impl GetPath for GenericParam {
+    fn get_path(&self) -> Option<syn::Path> {
+        match self {
+            GenericParam::Type(ty) => {
+                let ident = &ty.ident;
+                Some(parse_quote! { #ident })
+            }
+            _ => None,
+        }
+    }
+}
+
+impl GetPath for GenericArgument {
+    fn get_path(&self) -> Option<syn::Path> {
+        match self {
+            GenericArgument::Type(Type::Path(path)) => {
+                let path = &path.path;
+                Some(parse_quote! { #path })
+            }
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct CheckGenerics<'g, Generic> {
+    generics: &'g [&'g Generic],
+    used: Vec<&'g Generic>,
+}
+
+impl<'g, Generic> CheckGenerics<'g, Generic>
+where
+    Generic: GetPath + PartialEq,
+{
+    pub fn new(generics: &'g [&'g Generic]) -> Self {
         Self {
             generics,
             used: vec![],
         }
     }
 
-    pub fn used(self) -> Vec<&'g GenericParam> {
+    pub fn used(self) -> Vec<&'g Generic> {
         self.used
     }
 
     /// Returns split between used and unused generics
-    pub fn used_unused(self) -> (Vec<&'g GenericParam>, Vec<&'g GenericParam>) {
+    pub fn used_unused(self) -> (Vec<&'g Generic>, Vec<&'g Generic>) {
         let unused = self
             .generics
             .iter()
@@ -32,17 +63,18 @@ impl<'g> CheckGenerics<'g> {
     }
 }
 
-impl<'ast, 'g> Visit<'ast> for CheckGenerics<'g> {
+impl<'ast, 'g, Generic> Visit<'ast> for CheckGenerics<'g, Generic>
+where
+    Generic: GetPath + PartialEq,
+{
     fn visit_path(&mut self, p: &'ast syn::Path) {
-        if let Some(p) = p.get_ident() {
-            if let Some(gen) = self
-                .generics
-                .iter()
-                .find(|gen| matches!(gen, GenericParam::Type(ty) if ty.ident == *p))
-            {
-                if !self.used.contains(gen) {
-                    self.used.push(gen);
-                }
+        if let Some(gen) = self
+            .generics
+            .iter()
+            .find(|gen| gen.get_path().as_ref() == Some(p))
+        {
+            if !self.used.contains(gen) {
+                self.used.push(gen);
             }
         }
 
