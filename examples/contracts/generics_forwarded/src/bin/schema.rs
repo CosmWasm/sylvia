ba69d816e9e89b13fdc2483e4db00d979812ba8b
Jan Woźniak <jnwznk@gmail.com>
2023-11-22 14:57:57 +0100

    feat: Forward generics to custom_generic interface

--- examples/contracts/generics_forwarded/src/bin/schema.rs
+++ examples/contracts/generics_forwarded/src/bin/schema.rs
@@ -3,11 +3,11 @@ use cosmwasm_schema::write_api;
 #[cfg(not(tarpaulin_include))]
 fn main() {
     use generics_forwarded::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
-    use sylvia::types::SvCustomMsg;
+    use sylvia::types::{SvCustomMsg, SvCustomQuery};
 
     write_api! {
         instantiate: InstantiateMsg<SvCustomMsg>,
-        execute: ContractExecMsg<SvCustomMsg, SvCustomMsg>,
-        query: ContractQueryMsg<SvCustomMsg, SvCustomMsg>,
+        execute: ContractExecMsg<SvCustomMsg, SvCustomMsg, SvCustomMsg,SvCustomQuery>,
+        query: ContractQueryMsg<SvCustomMsg, SvCustomMsg, SvCustomMsg,SvCustomQuery>,
     }
 }
--- examples/contracts/generics_forwarded/src/custom_and_generic.rs
+++ examples/contracts/generics_forwarded/src/custom_and_generic.rs
@@ -1,14 +1,14 @@
 use cosmwasm_std::{CosmosMsg, Response, StdError, StdResult};
-use generic::Generic;
+use custom_and_generic::CustomAndGeneric;
 use serde::Deserialize;
 use sylvia::contract;
 use sylvia::types::{CustomMsg, CustomQuery, ExecCtx, QueryCtx, SvCustomMsg};
 
 #[contract(module = crate::contract)]
-#[messages(generic as Generic)]
+#[messages(custom_and_generic as CustomAndGeneric)]
 #[sv::custom(msg=CustomMsgT, query=CustomQueryT)]
 impl<InstantiateT, ExecT, QueryT, MigrateT, CustomMsgT, CustomQueryT, FieldT>
-    Generic<ExecT, QueryT, SvCustomMsg>
+    CustomAndGeneric<ExecT, QueryT, SvCustomMsg, CustomMsgT, CustomQueryT>
     for crate::contract::GenericsForwardedContract<
         InstantiateT,
         ExecT,
@@ -30,37 +30,44 @@ where
     type Error = StdError;
 
     #[msg(exec)]
-    fn generic_exec(&self, _ctx: ExecCtx, _msgs: Vec<CosmosMsg<ExecT>>) -> StdResult<Response> {
+    fn custom_generic_execute(
+        &self,
+        _ctx: ExecCtx<CustomQueryT>,
+        _msgs: Vec<CosmosMsg<ExecT>>,
+    ) -> StdResult<Response<CustomMsgT>> {
         Ok(Response::new())
     }
 
     #[msg(query)]
-    fn generic_query(&self, _ctx: QueryCtx, _msg: QueryT) -> StdResult<SvCustomMsg> {
+    fn custom_generic_query(
+        &self,
+        _ctx: QueryCtx<CustomQueryT>,
+        _msg: QueryT,
+    ) -> StdResult<SvCustomMsg> {
         Ok(SvCustomMsg {})
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::sv::test_utils::Generic;
+    use super::sv::test_utils::CustomAndGeneric;
     use crate::contract::sv::multitest_utils::CodeId;
-    use cosmwasm_std::CosmosMsg;
     use sylvia::multitest::App;
     use sylvia::types::{SvCustomMsg, SvCustomQuery};
 
     #[test]
     fn proxy_methods() {
         let app = App::<cw_multi_test::BasicApp<SvCustomMsg, SvCustomQuery>>::custom(|_, _, _| {});
-        let code_id: CodeId<
+        let code_id = CodeId::<
             SvCustomMsg,
             sylvia::types::SvCustomMsg,
             SvCustomMsg,
             SvCustomMsg,
-            sylvia::types::SvCustomMsg,
+            SvCustomMsg,
             SvCustomQuery,
             String,
             _,
-        > = CodeId::store_code(&app);
+        >::store_code(&app);
 
         let owner = "owner";
 
@@ -71,10 +78,7 @@ mod tests {
             .call(owner)
             .unwrap();
 
-        contract
-            .generic_exec(vec![CosmosMsg::Custom(SvCustomMsg {})])
-            .call(owner)
-            .unwrap();
-        contract.generic_query(SvCustomMsg).unwrap();
+        contract.custom_generic_execute(vec![]).call(owner).unwrap();
+        contract.custom_generic_query(SvCustomMsg {}).unwrap();
     }
 }
