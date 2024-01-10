8d0fead3378217ea7cbf91f60d91984317be06d3
Jan Woźniak <jnwznk@gmail.com>
2024-01-10 19:15:35 +0100

    feat: Impl interface with associated types on non generic contract

--- sylvia-derive/src/strip_self_path.rs
+++ sylvia-derive/src/strip_self_path.rs
@@ -1,5 +1,7 @@
 use syn::fold::Fold;
-use syn::Path;
+use syn::{parse_quote, Path};
+
+use crate::check_generics::GetPath;
 
 pub struct StripSelfPath;
 
@@ -11,5 +13,31 @@ impl Fold for StripSelfPath {
             .filter(|segment| segment.ident != "Self")
             .collect();
         syn::fold::fold_path(self, Path { segments, ..path })
+    }
+}
+
+pub struct AddSelfPath<'a, GenericT>(&'a [&'a GenericT]);
+
+impl<'a, GenericT> AddSelfPath<'a, GenericT> {
+    pub fn new(generics: &'a [&'a GenericT]) -> AddSelfPath<GenericT> {
+        AddSelfPath(generics)
+    }
+}
+
+impl<'a, GenericT> Fold for AddSelfPath<'a, GenericT>
+where
+    GenericT: GetPath,
+{
+    fn fold_path(&mut self, mut path: Path) -> Path {
+        if let Some(pos) = path.segments.iter().position(|segment| {
+            self.0.iter().any(|generic| match generic.get_path() {
+                Some(path) => Some(&segment.ident) == path.get_ident(),
+                None => false,
+            })
+        }) {
+            path.segments.insert(pos, parse_quote! { Self });
+        }
+
+        syn::fold::fold_path(self, path)
     }
 }
