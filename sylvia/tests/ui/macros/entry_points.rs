7b6d2ced10f0d91ebf72de45d6d0d017845c2ec9
Jan Woźniak <jnwznk@gmail.com>
2024-07-29 14:48:28 +0200

    chore: Cleanup in entry_points macro

--- sylvia/tests/ui/macros/entry_points.rs
+++ sylvia/tests/ui/macros/entry_points.rs
@@ -1,28 +1,62 @@
+#![allow(unused_imports)]
+
 use sylvia::cw_std::{Response, StdResult};
 use sylvia::types::{CustomMsg, CustomQuery, InstantiateCtx};
 use sylvia::{contract, entry_points};
 
-pub struct Contract<E, Q> {
-    _phantom: std::marker::PhantomData<(E, Q)>,
-}
+pub mod no_generics {
+    use super::*;
 
-#[entry_points]
-#[contract]
-#[sv::custom(msg = E, query = Q)]
-impl<E, Q> Contract<E, Q>
-where
-    E: CustomMsg + 'static,
-    Q: CustomQuery + 'static,
-{
-    pub fn new() -> Self {
-        Self {
-            _phantom: std::marker::PhantomData,
-        }
+    pub struct Contract<E, Q> {
+        _phantom: std::marker::PhantomData<(E, Q)>,
     }
 
-    #[sv::msg(instantiate)]
-    fn instantiate(&self, _ctx: InstantiateCtx<Q>) -> StdResult<Response<E>> {
-        Ok(Response::new())
+    #[entry_points]
+    #[contract]
+    #[sv::custom(msg = E, query = Q)]
+    impl<E, Q> Contract<E, Q>
+    where
+        E: CustomMsg + 'static,
+        Q: CustomQuery + 'static,
+    {
+        pub fn new() -> Self {
+            Self {
+                _phantom: std::marker::PhantomData,
+            }
+        }
+
+        #[sv::msg(instantiate)]
+        fn instantiate(&self, _ctx: InstantiateCtx<Q>) -> StdResult<Response<E>> {
+            Ok(Response::new())
+        }
+    }
+}
+
+pub mod missing_generics {
+    use super::*;
+
+    pub struct Contract<E, Q> {
+        _phantom: std::marker::PhantomData<(E, Q)>,
+    }
+
+    #[entry_points(generics<Empty>)]
+    #[contract]
+    #[sv::custom(msg = E, query = Q)]
+    impl<E, Q> Contract<E, Q>
+    where
+        E: CustomMsg + 'static,
+        Q: CustomQuery + 'static,
+    {
+        pub fn new() -> Self {
+            Self {
+                _phantom: std::marker::PhantomData,
+            }
+        }
+
+        #[sv::msg(instantiate)]
+        fn instantiate(&self, _ctx: InstantiateCtx<Q>) -> StdResult<Response<E>> {
+            Ok(Response::new())
+        }
     }
 }
 
