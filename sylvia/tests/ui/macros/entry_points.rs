5bde982cadcc35c304425d71f747434c3a63c0f9
Jan Woźniak <jnwznk@gmail.com>
2024-07-29 11:19:01 +0200

    test: Error if `entry_points` macro received no concrete types

---
+++ sylvia/tests/ui/macros/entry_points.rs
@@ -0,0 +1,29 @@
+use sylvia::cw_std::{Response, StdResult};
+use sylvia::types::{CustomMsg, CustomQuery, InstantiateCtx};
+use sylvia::{contract, entry_points};
+
+pub struct Contract<E, Q> {
+    _phantom: std::marker::PhantomData<(E, Q)>,
+}
+
+#[entry_points]
+#[contract]
+#[sv::custom(msg = E, query = Q)]
+impl<E, Q> Contract<E, Q>
+where
+    E: CustomMsg + 'static,
+    Q: CustomQuery + 'static,
+{
+    pub fn new() -> Self {
+        Self {
+            _phantom: std::marker::PhantomData,
+        }
+    }
+
+    #[sv::msg(instantiate)]
+    fn instantiate(&self, _ctx: InstantiateCtx<Q>) -> StdResult<Response<E>> {
+        Ok(Response::new())
+    }
+}
+
+fn main() {}
