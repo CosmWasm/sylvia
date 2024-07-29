07fcdc65f19cfce29473419c6609c70d11dddfc9
abefernan <44572727+abefernan@users.noreply.github.com>
2024-07-29 11:49:25 +0200

    test: Use concrete types in schema

--- examples/contracts/cw1-whitelist/src/bin/schema.rs
+++ examples/contracts/cw1-whitelist/src/bin/schema.rs
@@ -1,12 +1,14 @@
 use cosmwasm_schema::write_api;
+use cosmwasm_std::Empty;
 
 use cw1_whitelist::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+use cw1_whitelist::contract::SvCustomQuery;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
-        execute: ContractExecMsg,
-        query: ContractQueryMsg,
+        execute: ContractExecMsg<Empty, SvCustomQuery>,
+        query: ContractQueryMsg<Empty, SvCustomQuery>,
     }
 }
