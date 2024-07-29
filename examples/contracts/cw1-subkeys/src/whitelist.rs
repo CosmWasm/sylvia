b66185aa5fcbd3209cb435b6a4b1ca521c6341b9
abefernan <44572727+abefernan@users.noreply.github.com>
2024-07-29 13:37:19 +0200

    test: Generalize subkeys whitelist

--- examples/contracts/cw1-subkeys/src/whitelist.rs
+++ examples/contracts/cw1-subkeys/src/whitelist.rs
@@ -1,27 +1,35 @@
-use cosmwasm_std::{Empty, Response, StdResult};
-use sylvia::types::{ExecCtx, QueryCtx};
+use cosmwasm_std::{Response, StdResult};
+use sylvia::types::{CustomMsg, CustomQuery, ExecCtx, QueryCtx};
 use whitelist::responses::AdminListResponse;
 use whitelist::Whitelist;
 
 use crate::contract::Cw1SubkeysContract;
 use crate::error::ContractError;
 
-impl Whitelist for Cw1SubkeysContract {
+impl<E, Q> Whitelist for Cw1SubkeysContract<E, Q>
+where
+    E: CustomMsg + 'static,
+    Q: CustomQuery + 'static,
+{
     type Error = ContractError;
-    type ExecC = Empty;
-    type QueryC = Empty;
+    type ExecC = E;
+    type QueryC = Q;
 
-    fn freeze(&self, ctx: ExecCtx) -> Result<Response, Self::Error> {
+    fn freeze(&self, ctx: ExecCtx<Self::QueryC>) -> Result<Response<Self::ExecC>, Self::Error> {
         self.whitelist.freeze(ctx).map_err(From::from)
     }
 
-    fn update_admins(&self, ctx: ExecCtx, admins: Vec<String>) -> Result<Response, Self::Error> {
+    fn update_admins(
+        &self,
+        ctx: ExecCtx<Self::QueryC>,
+        admins: Vec<String>,
+    ) -> Result<Response<Self::ExecC>, Self::Error> {
         self.whitelist
             .update_admins(ctx, admins)
             .map_err(From::from)
     }
 
-    fn admin_list(&self, ctx: QueryCtx) -> StdResult<AdminListResponse> {
+    fn admin_list(&self, ctx: QueryCtx<Self::QueryC>) -> StdResult<AdminListResponse> {
         self.whitelist.admin_list(ctx)
     }
 }
