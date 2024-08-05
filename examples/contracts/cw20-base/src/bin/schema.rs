630df4819c8deaeb5ea4bf34d8c08e979cf8ab63
abefernan <44572727+abefernan@users.noreply.github.com>
2024-08-05 09:36:33 +0200

    test: Make schema generic

--- examples/contracts/cw20-base/src/bin/schema.rs
+++ examples/contracts/cw20-base/src/bin/schema.rs
@@ -1,12 +1,13 @@
 use cosmwasm_schema::write_api;
 
 use cw20_base::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+use cw20_base::contract::{SvCustomMsg, SvCustomQuery};
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
-        execute: ContractExecMsg,
-        query: ContractQueryMsg,
+        execute: ContractExecMsg<SvCustomMsg, SvCustomQuery>,
+        query: ContractQueryMsg<SvCustomMsg, SvCustomQuery>,
     }
 }
