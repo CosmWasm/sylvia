f9efc81e16f2dda8ce2431eba9b683161d74e9c6
abefernan <44572727+abefernan@users.noreply.github.com>
2024-08-05 09:37:43 +0200

    test: Make into_cosmos_msg generic

--- examples/contracts/cw20-base/src/responses.rs
+++ examples/contracts/cw20-base/src/responses.rs
@@ -18,7 +18,7 @@ impl Cw20ReceiveMsg {
     }
 
     /// creates a cosmos_msg sending this struct to the named contract
-    pub fn into_cosmos_msg<T: Into<String>>(self, contract_addr: T) -> StdResult<CosmosMsg> {
+    pub fn into_cosmos_msg<T: Into<String>, E>(self, contract_addr: T) -> StdResult<CosmosMsg<E>> {
         let msg = self.into_json_binary()?;
         let execute = WasmMsg::Execute {
             contract_addr: contract_addr.into(),
