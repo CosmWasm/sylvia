abfa2a562b6fe7b9dc94810c6a54b70678b65351
Tomasz Kulik <tomek.k@confio.gmbh>
2024-08-12 11:14:58 +0200

    chore!: Remove `InterfaceApi` in favor of `InterfaceMessagesApi` (#413)
    BREAKING CHANGE: Use `InterfaceMessagesApi` from the generated `sv` module instead

--- sylvia-derive/src/interface/communication/api.rs
+++ sylvia-derive/src/interface/communication/api.rs
@@ -1,6 +1,5 @@
-use crate::crate_module;
 use crate::parser::variant_descs::AsVariantDescs;
-use crate::parser::{Custom, MsgType};
+use crate::parser::MsgType;
 use crate::types::associated_types::{AssociatedTypes, ItemType};
 use crate::types::msg_variant::MsgVariants;
 use proc_macro2::TokenStream;
@@ -9,28 +8,20 @@ use syn::ItemTrait;
 
 pub struct Api<'a> {
     source: &'a ItemTrait,
-    custom: &'a Custom,
     associated_types: &'a AssociatedTypes<'a>,
 }
 
 impl<'a> Api<'a> {
-    pub fn new(
-        source: &'a ItemTrait,
-        associated_types: &'a AssociatedTypes<'a>,
-        custom: &'a Custom,
-    ) -> Self {
+    pub fn new(source: &'a ItemTrait, associated_types: &'a AssociatedTypes<'a>) -> Self {
         Self {
             source,
-            custom,
             associated_types,
         }
     }
 
     pub fn emit(&self) -> TokenStream {
-        let sylvia = crate_module();
         let Self {
             source,
-            custom,
             associated_types,
         } = self;
 
@@ -58,45 +49,27 @@ impl<'a> Api<'a> {
             &source.generics.where_clause,
         );
 
-        let where_clause = &self.associated_types.as_where_clause();
-        let custom_query = custom.query_or_default();
         let exec_generics = &exec_variants.used_generics();
         let query_generics = &query_variants.used_generics();
         let sudo_generics = &sudo_variants.used_generics();
-
-        let phantom = if !generics.is_empty() {
-            quote! {
-                _phantom: std::marker::PhantomData<( #(#generics,)* )>,
-            }
-        } else {
-            quote! {}
-        };
 
         quote! {
             pub trait InterfaceMessagesApi {
                 type Exec;
                 type Query;
                 type Sudo;
-                type Querier<'querier>;
             }
 
             impl<Contract: #interface_name> InterfaceMessagesApi for Contract {
                 type Exec = ExecMsg < #(<Contract as #interface_name >:: #exec_generics,)* >;
                 type Query = QueryMsg < #(<Contract as #interface_name >:: #query_generics,)* >;
                 type Sudo = SudoMsg < #(<Contract as #interface_name >:: #sudo_generics ,)* >;
-                type Querier<'querier> = #sylvia ::types::BoundQuerier<'querier, #custom_query, Contract >;
             }
 
-
-            pub struct Api < #(#generics,)* > {
-                #phantom
-            }
-
-            impl < #(#generics,)* > #sylvia ::types::InterfaceApi for Api < #(#generics,)* > #where_clause {
+            impl<'sv_iface_msg_api, Error, #(#generics),*> InterfaceMessagesApi for dyn #interface_name < Error = Error, #(#generics = #generics,)* > + 'sv_iface_msg_api {
                 type Exec = ExecMsg < #(#exec_generics,)* >;
                 type Query = QueryMsg < #(#query_generics,)* >;
                 type Sudo = SudoMsg < #(#sudo_generics,)* >;
-                type Querier<'querier, Contract> = #sylvia ::types::BoundQuerier<'querier, #custom_query, Contract >;
             }
         }
     }
