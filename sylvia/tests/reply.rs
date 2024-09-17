67085e55839b7c0611fc5d567013e8d6cf986a9c
Jan Woźniak <jnwznk@gmail.com>
2024-09-17 14:17:35 +0200

    feat: Add InstantiateBuilder and SubMsg trait

--- sylvia/tests/reply.rs
+++ sylvia/tests/reply.rs
@@ -1,39 +1,52 @@
 #![cfg(feature = "sv_replies")]
 
-use cosmwasm_std::SubMsgResult;
+use cosmwasm_std::{Empty, SubMsgResult};
 use cw_storage_plus::Item;
 use cw_utils::{parse_instantiate_response_data, ParseReplyError};
-use noop_contract::sv::Executor;
-use sv::{ALWAYS_REPLY_ID, FAILURE_REPLY_ID, REMOTE_INSTANTIATED_REPLY_ID, SUCCESS_REPLY_ID};
-use sylvia::cw_std::{to_json_binary, Addr, Binary, ReplyOn, Response, StdError, SubMsg, WasmMsg};
-use sylvia::types::{ExecCtx, InstantiateCtx, QueryCtx, Remote, ReplyCtx};
+use noop_contract::sv::{Executor, NoopContractInstantiateBuilder};
+use sv::{
+    SubMsgMethods, ALWAYS_REPLY_ID, FAILURE_REPLY_ID, REMOTE_INSTANTIATED_REPLY_ID,
+    SUCCESS_REPLY_ID,
+};
+use sylvia::builder::instantiate::InstantiateBuilder;
+use sylvia::cw_std::{Addr, Binary, Response, StdError, SubMsg};
+use sylvia::types::{CustomMsg, CustomQuery, ExecCtx, InstantiateCtx, QueryCtx, Remote, ReplyCtx};
 use sylvia::{contract, entry_points};
 use thiserror::Error;
 
 #[allow(dead_code)]
 mod noop_contract {
-    use cosmwasm_std::{StdError, StdResult};
-    use sylvia::types::{ExecCtx, InstantiateCtx};
+    use cosmwasm_std::{Empty, StdError, StdResult};
+    use sylvia::types::{CustomMsg, CustomQuery, ExecCtx, InstantiateCtx};
     use sylvia::{contract, entry_points};
 
     use sylvia::cw_std::Response;
 
-    pub struct NoopContract;
+    pub struct NoopContract<M, Q> {
+        _phantom: std::marker::PhantomData<(M, Q)>,
+    }
 
-    #[entry_points]
+    #[entry_points(generics<Empty, Empty>)]
     #[contract]
-    impl NoopContract {
+    #[sv::custom(msg=M, query=Q)]
+    impl<M, Q> NoopContract<M, Q>
+    where
+        M: CustomMsg + 'static,
+        Q: CustomQuery + 'static,
+    {
         pub const fn new() -> Self {
-            Self
+            Self {
+                _phantom: std::marker::PhantomData,
+            }
         }
 
         #[sv::msg(instantiate)]
-        fn instantiate(&self, _ctx: InstantiateCtx) -> StdResult<Response> {
+        fn instantiate(&self, _ctx: InstantiateCtx<Q>) -> StdResult<Response<M>> {
             Ok(Response::new())
         }
 
         #[sv::msg(exec)]
-        fn noop(&self, _ctx: ExecCtx, should_fail: bool) -> StdResult<Response> {
+        fn noop(&self, _ctx: ExecCtx<Q>, should_fail: bool) -> StdResult<Response<M>> {
             if should_fail {
                 Err(StdError::generic_err("Failed as requested"))
             } else {
@@ -52,72 +65,141 @@ pub enum ContractError {
     ParseReply(#[from] ParseReplyError),
 }
 
-pub struct Contract {
-    remote: Item<Remote<'static, noop_contract::NoopContract>>,
+pub struct Contract<M, Q> {
+    remote: Item<Remote<'static, noop_contract::NoopContract<Empty, Empty>>>,
     last_reply: Item<u64>,
+    _phantom: std::marker::PhantomData<(M, Q)>,
 }
 
-#[entry_points]
+#[entry_points(generics<Empty, Empty>)]
 #[contract]
 #[sv::error(ContractError)]
-impl Contract {
+#[sv::custom(msg=M, query=Q)]
+impl<M, Q> Contract<M, Q>
+where
+    M: CustomMsg + 'static,
+    Q: CustomQuery + 'static,
+{
     pub fn new() -> Self {
         Self {
             remote: Item::new("remote"),
             last_reply: Item::new("last_reply"),
+            _phantom: std::marker::PhantomData,
         }
     }
 
     #[sv::msg(instantiate)]
     pub fn instantiate(
         &self,
-        _ctx: InstantiateCtx,
+        _ctx: InstantiateCtx<Q>,
         remote_code_id: u64,
-    ) -> Result<Response, ContractError> {
-        let msg = WasmMsg::Instantiate {
-            admin: None,
-            code_id: remote_code_id,
-            msg: to_json_binary(&noop_contract::sv::InstantiateMsg::new())?,
-            funds: vec![],
-            label: "noop".to_string(),
-        };
-        let sub_msg = SubMsg::reply_on_success(msg, REMOTE_INSTANTIATED_REPLY_ID);
+    ) -> Result<Response<M>, ContractError> {
+        let sub_msg = InstantiateBuilder::noop_contract(remote_code_id)?
+            .with_label("noop")
+            .build()
+            .remote_instantiated();
         Ok(Response::new().add_submessage(sub_msg))
     }
 
     #[sv::msg(exec)]
-    pub fn call_remote(
+    pub fn call_remote_success(
         &self,
-        ctx: ExecCtx,
-        reply_id: u64,
+        ctx: ExecCtx<Q>,
         should_fail: bool,
-        reply_on: ReplyOn,
-    ) -> Result<Response, ContractError> {
+    ) -> Result<Response<M>, ContractError> {
+        let msg = self
+            .remote
+            .load(ctx.deps.storage)?
+            .executor()
+            .noop(should_fail)?
+            .build()
+            .success();
+
+        Ok(Response::new().add_submessage(msg))
+    }
+
+    #[sv::msg(exec)]
+    pub fn call_remote_failure(
+        &self,
+        ctx: ExecCtx<Q>,
+        should_fail: bool,
+    ) -> Result<Response<M>, ContractError> {
+        let msg = self
+            .remote
+            .load(ctx.deps.storage)?
+            .executor()
+            .noop(should_fail)?
+            .build()
+            .failure();
+
+        Ok(Response::new().add_submessage(msg))
+    }
+
+    #[sv::msg(exec)]
+    pub fn call_remote_both(
+        &self,
+        ctx: ExecCtx<Q>,
+        should_fail: bool,
+    ) -> Result<Response<M>, ContractError> {
+        let msg = self
+            .remote
+            .load(ctx.deps.storage)?
+            .executor()
+            .noop(should_fail)?
+            .build()
+            .both();
+
+        Ok(Response::new().add_submessage(msg))
+    }
+
+    #[sv::msg(exec)]
+    pub fn call_remote_always(
+        &self,
+        ctx: ExecCtx<Q>,
+        should_fail: bool,
+    ) -> Result<Response<M>, ContractError> {
+        let msg = self
+            .remote
+            .load(ctx.deps.storage)?
+            .executor()
+            .noop(should_fail)?
+            .build()
+            .always();
+
+        Ok(Response::new().add_submessage(msg))
+    }
+
+    #[sv::msg(exec)]
+    pub fn call_remote_unknown_id(
+        &self,
+        ctx: ExecCtx<Q>,
+        should_fail: bool,
+        reply_id: u64,
+    ) -> Result<Response<M>, ContractError> {
         let msg = self
             .remote
             .load(ctx.deps.storage)?
             .executor()
             .noop(should_fail)?
             .build();
-        let mut msg = SubMsg::new(msg);
-        msg.id = reply_id;
-        msg.reply_on = reply_on;
+
+        let msg = SubMsg::reply_always(msg, reply_id);
 
         Ok(Response::new().add_submessage(msg))
     }
 
     #[sv::msg(query)]
-    pub fn last_reply(&self, ctx: QueryCtx) -> Result<u64, ContractError> {
+    pub fn last_reply(&self, ctx: QueryCtx<Q>) -> Result<u64, ContractError> {
         self.last_reply.load(ctx.deps.storage).map_err(Into::into)
     }
 
     #[sv::msg(reply, reply_on=success)]
     fn remote_instantiated(
         &self,
-        ctx: ReplyCtx,
+        ctx: ReplyCtx<Q>,
         data: Option<Binary>,
         _payload: Binary,
-    ) -> Result<Response, ContractError> {
+    ) -> Result<Response<M>, ContractError> {
         self.last_reply
             .save(ctx.deps.storage, &REMOTE_INSTANTIATED_REPLY_ID)?;
         let init_data = parse_instantiate_response_data(&data.unwrap())?;
@@ -132,10 +214,10 @@ impl Contract {
     #[sv::msg(reply, handlers=[success, both], reply_on=success)]
     fn success(
         &self,
-        ctx: ReplyCtx,
+        ctx: ReplyCtx<Q>,
         _data: Option<Binary>,
         _payload: Binary,
-    ) -> Result<Response, ContractError> {
+    ) -> Result<Response<M>, ContractError> {
         self.last_reply.save(ctx.deps.storage, &SUCCESS_REPLY_ID)?;
 
         Ok(Response::new())
@@ -144,10 +226,10 @@ impl Contract {
     #[sv::msg(reply, handlers=[failure, both], reply_on=failure)]
     fn failure(
         &self,
-        ctx: ReplyCtx,
+        ctx: ReplyCtx<Q>,
         _error: String,
         _payload: Binary,
-    ) -> Result<Response, ContractError> {
+    ) -> Result<Response<M>, ContractError> {
         self.last_reply.save(ctx.deps.storage, &FAILURE_REPLY_ID)?;
 
         Ok(Response::new())
@@ -156,10 +238,10 @@ impl Contract {
     #[sv::msg(reply, reply_on=always)]
     fn always(
         &self,
-        ctx: ReplyCtx,
+        ctx: ReplyCtx<Q>,
         _result: SubMsgResult,
         _payload: Binary,
-    ) -> Result<Response, ContractError> {
+    ) -> Result<Response<M>, ContractError> {
         self.last_reply.save(ctx.deps.storage, &ALWAYS_REPLY_ID)?;
 
         Ok(Response::new())
@@ -170,12 +252,11 @@ mod tests {
     use crate::noop_contract::sv::mt::CodeId as NoopCodeId;
     use crate::sv::mt::{CodeId, ContractProxy};
     use crate::sv::{
-        ALWAYS_REPLY_ID, BOTH_REPLY_ID, FAILURE_REPLY_ID, REMOTE_INSTANTIATED_REPLY_ID,
-        SUCCESS_REPLY_ID,
+        ALWAYS_REPLY_ID, FAILURE_REPLY_ID, REMOTE_INSTANTIATED_REPLY_ID, SUCCESS_REPLY_ID,
     };
 
     use sylvia::cw_multi_test::IntoBech32;
-    use sylvia::cw_std::{ReplyOn, StdError};
+    use sylvia::cw_std::StdError;
     use sylvia::multitest::App;
 
     #[test]
@@ -196,74 +277,50 @@ mod tests {
         assert_eq!(last_reply, REMOTE_INSTANTIATED_REPLY_ID);
 
         // Should not dispatch if expected success and execution failed
-        let err = contract
-            .call_remote(SUCCESS_REPLY_ID, true, ReplyOn::Success)
-            .call(&owner)
-            .unwrap_err();
+        let err = contract.call_remote_success(true).call(&owner).unwrap_err();
         assert_eq!(err, StdError::generic_err("Failed as requested").into());
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, REMOTE_INSTANTIATED_REPLY_ID);
 
         // Should dispatch if expected success and execution succeeded
-        contract
-            .call_remote(SUCCESS_REPLY_ID, false, ReplyOn::Success)
-            .call(&owner)
-            .unwrap();
+        contract.call_remote_success(false).call(&owner).unwrap();
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, SUCCESS_REPLY_ID);
 
         // Should not dispatch if expected failure and execution succeeded
-        contract
-            .call_remote(FAILURE_REPLY_ID, false, ReplyOn::Error)
-            .call(&owner)
-            .unwrap();
+        contract.call_remote_failure(false).call(&owner).unwrap();
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, SUCCESS_REPLY_ID);
 
         // Should dispatch if expected failure and execution failed
-        contract
-            .call_remote(FAILURE_REPLY_ID, true, ReplyOn::Error)
-            .call(&owner)
-            .unwrap();
+        contract.call_remote_failure(true).call(&owner).unwrap();
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, FAILURE_REPLY_ID);
 
         // Should dispatch if expected any result and execution succeeded
-        contract
-            .call_remote(ALWAYS_REPLY_ID, false, ReplyOn::Always)
-            .call(&owner)
-            .unwrap();
-        let last_reply = contract.last_reply().unwrap();
-        assert_eq!(last_reply, ALWAYS_REPLY_ID);
-
-        // Should dispatch if expected any result and execution failed
-        contract
-            .call_remote(ALWAYS_REPLY_ID, true, ReplyOn::Always)
-            .call(&owner)
-            .unwrap();
+        contract.call_remote_always(false).call(&owner).unwrap();
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, ALWAYS_REPLY_ID);
 
         // Should dispatch if expected both results and execution succeeded
-        contract
-            .call_remote(BOTH_REPLY_ID, false, ReplyOn::Success)
-            .call(&owner)
-            .unwrap();
+        contract.call_remote_both(false).call(&owner).unwrap();
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, SUCCESS_REPLY_ID);
 
+        // Should dispatch if expected any result and execution failed
+        contract.call_remote_always(true).call(&owner).unwrap();
+        let last_reply = contract.last_reply().unwrap();
+        assert_eq!(last_reply, ALWAYS_REPLY_ID);
+
         // Should dispatch if expected both results and execution failed
-        contract
-            .call_remote(BOTH_REPLY_ID, true, ReplyOn::Error)
-            .call(&owner)
-            .unwrap();
+        contract.call_remote_both(true).call(&owner).unwrap();
         let last_reply = contract.last_reply().unwrap();
         assert_eq!(last_reply, FAILURE_REPLY_ID);
 
         // Should return error if unknown reply ID received
         let unknown_reply_id = 42u64;
         let err = contract
-            .call_remote(unknown_reply_id, false, ReplyOn::Success)
+            .call_remote_unknown_id(false, unknown_reply_id)
             .call(&owner)
             .unwrap_err();
         assert_eq!(
