9c351623fffd174968670f5ff30651a4d581e5dd
Jan Woźniak <jnwznk@gmail.com>
2024-10-21 16:31:26 +0200

    feat: Add `instantiate` parameter to the `sv::data` attribute

--- sylvia-derive/src/parser/attributes/data.rs
+++ sylvia-derive/src/parser/attributes/data.rs
@@ -1,5 +1,6 @@
 use proc_macro_error::emit_error;
 use syn::parse::{Parse, ParseStream, Parser};
+use syn::spanned::Spanned;
 use syn::{Error, Ident, MetaList, Result, Token};
 
 /// Type wrapping data parsed from `sv::data` attribute.
@@ -7,16 +8,27 @@ use syn::{Error, Ident, MetaList, Result, Token};
 pub struct DataFieldParams {
     pub raw: bool,
     pub opt: bool,
+    pub instantiate: bool,
 }
 
 impl DataFieldParams {
     pub fn new(attr: &MetaList) -> Result<Self> {
-        DataFieldParams::parse
+        let data = DataFieldParams::parse
             .parse2(attr.tokens.clone())
             .map_err(|err| {
                 emit_error!(err.span(), err);
                 err
-            })
+            })?;
+
+        if data.instantiate && data.raw {
+            emit_error!(
+                attr.tokens.span(),
+                "The `instantiate` cannot be used in pair with `raw` parameter.";
+                note = "Use any combination of [`raw`, `opt`] or [`instantiate`, `opt`] pairs."
+            );
+        }
+
+        Ok(data)
     }
 }
 
@@ -29,11 +41,12 @@ impl Parse for DataFieldParams {
             match option.to_string().as_str() {
                 "raw" => data.raw = true,
                 "opt" => data.opt = true,
+                "instantiate" => data.instantiate = true,
                 _ => {
                     return Err(Error::new(
                         option.span(),
                         "Invalid data parameter.\n
-  = note: Expected one of [`raw`, `opt`] comma separated.\n",
+  = note: Expected one of [`raw`, `opt`, `instantiate`] comma separated.\n",
                     ))
                 }
             }
