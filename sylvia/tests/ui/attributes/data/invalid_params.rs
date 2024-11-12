aa3fd6ff603a5e91efe11fbe93e92389a1e9ea2a
Jan Woźniak <jnwznk@gmail.com>
2024-11-12 10:11:52 +0100

    test: Check error if `instantiate` and `raw` used in `data`

--- sylvia/tests/ui/attributes/data/invalid_params.rs
+++ sylvia/tests/ui/attributes/data/invalid_params.rs
@@ -4,28 +4,61 @@ use sylvia::contract;
 use sylvia::ctx::{InstantiateCtx, ReplyCtx};
 use sylvia::cw_std::{Binary, Reply, Response, StdResult};
 
-pub struct Contract;
+pub mod invalid_data {
+    use super::*;
 
-#[contract]
-#[sv::features(replies)]
-impl Contract {
-    pub fn new() -> Self {
-        Self
+    pub struct Contract;
+
+    #[contract]
+    #[sv::features(replies)]
+    impl Contract {
+        pub fn new() -> Self {
+            Self
+        }
+
+        #[sv::msg(instantiate)]
+        pub fn instantiate(&self, _ctx: InstantiateCtx) -> StdResult<Response> {
+            Ok(Response::new())
+        }
+
+        #[sv::msg(reply, reply_on=success)]
+        fn reply(
+            &self,
+            _ctx: ReplyCtx,
+            #[sv::data(invalid)] _data: Option<Binary>,
+            _param: String,
+        ) -> StdResult<Response> {
+            Ok(Response::new())
+        }
     }
+}
 
-    #[sv::msg(instantiate)]
-    pub fn instantiate(&self, _ctx: InstantiateCtx) -> StdResult<Response> {
-        Ok(Response::new())
-    }
+pub mod instantiate_and_raw {
+    use super::*;
 
-    #[sv::msg(reply, reply_on=success)]
-    fn reply(
-        &self,
-        _ctx: ReplyCtx,
-        #[sv::data(invalid)] _data: Option<Binary>,
-        _param: String,
-    ) -> StdResult<Response> {
-        Ok(Response::new())
+    pub struct Contract;
+
+    #[contract]
+    #[sv::features(replies)]
+    impl Contract {
+        pub fn new() -> Self {
+            Self
+        }
+
+        #[sv::msg(instantiate)]
+        pub fn instantiate(&self, _ctx: InstantiateCtx) -> StdResult<Response> {
+            Ok(Response::new())
+        }
+
+        #[sv::msg(reply, reply_on=success)]
+        fn reply(
+            &self,
+            _ctx: ReplyCtx,
+            #[sv::data(instantiate, raw)] _data: Option<Binary>,
+            _param: String,
+        ) -> StdResult<Response> {
+            Ok(Response::new())
+        }
     }
 }
 
