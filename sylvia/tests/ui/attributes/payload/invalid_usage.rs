ceb476c4a30d33a85da4dfed9197742b0e043e80
Jan Woźniak <jnwznk@gmail.com>
2024-11-15 15:07:08 +0100

    feat: Error on wrong `sv::data` and `sv::payload` usage

---
+++ sylvia/tests/ui/attributes/payload/invalid_usage.rs
@@ -0,0 +1,29 @@
+#![allow(unused_imports, unused_variables)]
+use sylvia::ctx::{InstantiateCtx, ReplyCtx};
+use sylvia::cw_std::{Addr, Binary, Response, StdResult};
+
+pub mod used_on_context {
+    use super::*;
+
+    pub struct Contract {}
+
+    #[sylvia::contract]
+    #[sv::features(replies)]
+    impl Contract {
+        pub const fn new() -> Self {
+            Self {}
+        }
+
+        #[sv::msg(instantiate)]
+        fn instantiate(&self, _ctx: InstantiateCtx) -> StdResult<Response> {
+            Ok(Response::new())
+        }
+
+        #[sv::msg(reply, reply_on=success)]
+        fn reply(&self, #[sv::payload(raw)] _ctx: ReplyCtx) -> StdResult<Response> {
+            Ok(Response::new())
+        }
+    }
+}
+
+fn main() {}
