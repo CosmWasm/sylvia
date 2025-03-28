b6f937db1f0a5576d5b89b1c5a75343d5a53bf11
Jan Woźniak <jnwznk@gmail.com>
2025-03-28 09:34:28 +0100

    build: Exit nonzero `ci_contracts` if it fails on CI

--- ci_contracts.sh
+++ ci_contracts.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
 # Run command passed as argument to the script and run it for every directory
 # found in path.
 # Intended usage: while in `examples/contracts` directory, run
@@ -7,9 +10,9 @@
 command=${1}
 
 for contract in */; do
-	echo "Running ${command} for ${contract}"
+    echo "Running ${command} for ${contract}"
 
-	cd $contract
-	$command
-	cd -
+    cd $contract
+    $command
+    cd -
 done
