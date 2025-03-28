cb8bde7d891849d71a5d35ad392e390b265267f7
Jan Woźniak <jnwznk@gmail.com>
2025-03-28 09:51:07 +0100

    fix: Set custom path for remaining `entry-point` macros

--- examples/contracts/entry-points-overriding/src/entry_points.rs
+++ examples/contracts/entry-points-overriding/src/entry_points.rs
@@ -1,15 +1,17 @@
-use sylvia::cw_std::{entry_point, DepsMut, Env, MessageInfo, Response, StdResult};
+#[cfg(not(feature = "library"))]
+use sylvia::cw_std::entry_point;
+use sylvia::cw_std::{DepsMut, Env, MessageInfo, Response, StdResult};
 
 use crate::contract::CounterContract;
 use crate::messages::{CustomExecMsg, SudoMsg};
 
-#[entry_point]
+#[cfg_attr(not(feature = "library"), entry_point(crate = "sylvia::cw_std"))]
 pub fn sudo(deps: DepsMut, _env: Env, _msg: SudoMsg) -> StdResult<Response> {
     CounterContract::new().counter.save(deps.storage, &3)?;
     Ok(Response::new())
 }
 
-#[entry_point]
+#[cfg_attr(not(feature = "library"), entry_point(crate = "sylvia::cw_std"))]
 pub fn execute(
     deps: DepsMut,
     env: Env,
